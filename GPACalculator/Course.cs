96e28b75ed67de5be8c2506131c9b0986bfabfbb
minziee <devin.albritton@gmail.com>
2018-07-22 11:06:37 -0400

    Created Classes for main menu, indiviual course, and calculator

---
+++ GPACalculator/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPACalculator
+{
+    public class Course
+    {
+        public int Grade { get; private set; }
+        public int Credits { get; private set; }
+        private string CourseTitle { get; set; }
+        public int WeightedGrade;
+
+        public Course()
+        {}
+        public Course(int Grade, int Credits, string CourseTitle)
+        {
+            this.Grade = Grade;
+            this.Credits = Credits;
+            this.CourseTitle = CourseTitle;
+            WeightedGrade = this.Grade * this.Credits;
+        }
+
+
+    }
+}
